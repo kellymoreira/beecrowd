7c2f5895c1c49922b7feca3489af433b74b4022e
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-10-05 19:00:22 -0300

    Update 1000.js

--- Solutions/HelloWorld!/1000.js
+++ Solutions/HelloWorld!/1000.js
@@ -3,7 +3,7 @@
 /********************************************************************************
 * 
 * Your first program in any programming language is usually "Hello World!". 
-* In this first problem all you have to do * is print this message on the screen.
+* In this first problem all you have to do is print this message on the screen.
 *               
 *********************************************************************************/
 

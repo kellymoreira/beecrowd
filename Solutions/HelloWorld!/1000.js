12c3cea4dee9eea12c32700663223c0f6d99edf6
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-10-05 16:53:13 -0300

    Add files via upload

---
+++ Solutions/HelloWorld!/1000.js
@@ -0,0 +1,10 @@
+// HELLO WORLD!
+
+/********************************************************************************** 
+* 
+* Your first program in any programming language is usually "Hello World!". 
+* In this first problem all you have to do * is print this message on the screen.
+*               
+**********************************************************************************/
+
+console.log("Hello World!");

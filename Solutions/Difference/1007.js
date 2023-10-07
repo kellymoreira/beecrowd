c71e4f4a2d1ae605436c52981150d8824fdad437
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-10-07 16:01:49 -0300

    New Solution (1007 -  Difference)

--- Solutions/Difference/1007.js
+++ Solutions/Difference/1007.js
@@ -2,7 +2,7 @@
 
 /**************************************************************************************************************
 * 
-* Read four integer values named A, B, C and D. 
+* Read four integer values named A, B, C, and D. 
 * Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
 *
 * INPUT

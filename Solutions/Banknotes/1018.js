b76706145316d0793b45c677a693c568a70e01c7
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-10-08 16:15:56 -0300

    New Solution (1018 -  Banknotes)

--- Solutions/Banknotes/1018.js
+++ Solutions/Banknotes/1018.js
@@ -1,16 +1,19 @@
 // BANKNOTES
 
-/********************************************************************************************************************
+/************************************************************************************************************************************************
 * 
-* In this problem you have to read an integer value and calculate the smallest possible number of banknotes in which the value may be decomposed. The possible banknotes are 100, 50, 20, 10, 5, 2 and 1. Print the read value and the list of banknotes.
-
-Input
-The input file contains an integer value N (0 < N < 1000000).
-
-Output
-Print the read number and the minimum quantity of each necessary banknotes in Portuguese language, as the given example. Do not forget to print the end of line after each line, otherwise you will receive “Presentation Error”.
+* In this problem you have to read an integer value and calculate the smallest possible number of banknotes in which the value may be decomposed. 
+* The possible banknotes are 100, 50, 20, 10, 5, 2 and 1. 
+* Print the read value and the list of banknotes.
 *
-********************************************************************************************************************/
+* INPUT
+* The input file contains an integer value N (0 < N < 1000000).
+*
+* OUTPUT
+* Print the read number and the minimum quantity of each necessary banknote in Portuguese language, as in the given example. 
+* Do not forget to print the end of the line after each line, otherwise, you will receive a “Presentation Error”.
+*
+************************************************************************************************************************************************/
 
 const fileSystem = require('fs');
 const endereco = require('path');
@@ -49,8 +52,10 @@ Let's say integerValue is initially 47, and notes is 10.
 
 integerValue %= notes; would mean "47 modulo 10," which is 7 because 47 divided by 10 equals 4 with a remainder of 7.
 
-So, after this line of code executes, integerValue would be updated to 7. This is done to keep track of the remaining amount after using the current banknote denomination to represent part of the original value.
+So, after this line of code executes, integerValue would be updated to 7. 
+This is done to keep track of the remaining amount after using the current banknote denomination to represent part of the original value.
 
-The %= operator is just a shorthand way of performing the modulo operation and updating the variable in a single step. It's equivalent to writing integerValue = integerValue % notes;.
+The %= operator is just a shorthand way of performing the modulo operation and updating the variable in a single step. 
+It's equivalent to writing integerValue = integerValue % notes;.
 
 */

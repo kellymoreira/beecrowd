22521200f1ea0b6e5b51b409df22b960e8da6b5c
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-10-09 01:13:16 -0300

    New Solution (1046 - Game Time)

--- Solutions/GameTime/1046.js
+++ Solutions/GameTime/1046.js
@@ -5,7 +5,7 @@
 * Read the start time and end time of a game, in hours.
 * Then calculate the duration of the game, knowing that the game can begin in a day and finish in another day, 
 * with a maximum duration of 24 hours. 
-* The message must be printed in portuguese “O JOGO DUROU X HORA(S)” that means “THE GAME LASTED X HOUR(S)”
+* The message must be printed in Portuguese “O JOGO DUROU X HORA(S)” which means “THE GAME LASTED X HOUR(S)”
 *
 * INPUT
 * Two integer numbers representing the start and end time of a game.
@@ -14,6 +14,7 @@
 * Print the duration of the game as in the sample output.
 *
 ******************************************************************************************************************/
+
 const fileSystem = require('fs');
 const endereco = require('path');
 const enderecoDesteScriptJS = endereco.dirname(process.argv[1]);

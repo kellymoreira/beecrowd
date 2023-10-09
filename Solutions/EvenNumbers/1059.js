fa5cf7a2607ba506bd2077946f722a0f2ad87477
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-10-09 00:08:35 -0300

    Add files via upload

---
+++ Solutions/EvenNumbers/1059.js
@@ -0,0 +1,49 @@
+// EVEN NUMBER
+
+/*****************************************************************************************************************
+* 
+* Write a program that prints all even numbers between 1 and 100, including them if it is the case.
+*
+* INPUT
+* In this extremely simple problem there is no input.
+*
+* OUTPUT
+* Print all even numbers between 1 and 100, including them, one by row.
+*
+*****************************************************************************************************************/
+
+
+const fileSystem = require('fs');
+const endereco = require('path');
+const enderecoDesteScriptJS = endereco.dirname(process.argv[1]);
+const conteudoArquivo = fileSystem.readFileSync(enderecoDesteScriptJS + '\\dev\\stdin', 'utf8');
+const quebraLinhaWindows = '\r\n'
+
+let lines = conteudoArquivo.split(quebraLinhaWindows);
+
+// COPY AND PASTE INTO BEECROWD FROM THIS POINT ON:
+
+// In this extremely simple problem there is no input.
+
+// Write a program that prints all even numbers between 1 and 100, including them , one by row.
+
+for (let number = 2; number <= 100; number += 2) {
+    console.log(number);
+}
+
+/*
+1. we start by declaring a variable named number and initializing it with the value 2. this variable will represent the current even number that we want to print.
+
+2. next, we have a for loop, which is a control structure used for iteration. the loop is defined with the following components:
+
+INITIALIZATION: let number = 2 sets the initial value of number to 2.
+CONDITION: number <= 100 specifies that the loop should continue as long as the value of number is less than or equal to 100.
+ITERATION: number += 2 increments the value of number by 2 in each iteration.
+
+3. inside the loop, we have a console.log(number) statement. this statement is responsible for printing the current value of number to the console.
+
+4. in each iteration of the loop, the value of number is printed to the console, and it represents an even number between 2 and 100.
+
+5. the loop continues to execute until the condition number <= 100 is no longer met, at which point the loop terminates.
+
+*/

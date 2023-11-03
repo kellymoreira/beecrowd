91c71138b1171915efc8e06af71c78285c32bbfe
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-11-03 01:05:13 -0300

    new solution to 2602 - Basic Select

--- Solutions-SQL/BasicSelect/2602.sql
+++ Solutions-SQL/BasicSelect/2602.sql
@@ -17,6 +17,6 @@ VALUES
   (4,	'Jane Ester',	'Av 7 de setembro',	'Erechim',	'RS',	800.00),
   (5, 'Marcos Antônio dos Santos',	'Av Farrapos',	'Porto Alegre',	'RS',	4250.25);
   
--- COPY AND PASTE INTO BEECROWD FROM THIS POINT ON:
+-- COPY AND PASTE INTO BEECROWD FROM THIS POINT ON --
 
  SELECT name FROM customers WHERE state = 'RS'

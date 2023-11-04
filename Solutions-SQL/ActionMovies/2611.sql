64f941422531227830abab333b573d5df6dab586
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-11-04 14:08:19 -0300

    new solution to 2611 - Action Movies

--- Solutions-SQL/ActionMovies/2611.sql
+++ Solutions-SQL/ActionMovies/2611.sql
@@ -1,1 +1,35 @@
+/* ACTION MOVIES */
 
+CREATE TABLE genres (
+  id numeric PRIMARY KEY,
+  description varchar(50)
+);
+
+CREATE TABLE movies (
+  id numeric PRIMARY KEY,
+  name varchar(50),
+  id_genres numeric REFERENCES genres (id)
+);
+
+INSERT INTO genres (id, description)
+VALUES
+  (1,	'Animation'),
+  (2,	'Horror'),
+  (3,	'Action'),
+  (4,	'Drama'),
+  (5,	'Comedy');
+  
+INSERT INTO movies (id, name, id_genres)
+VALUES
+  (1,	'Batman',	3),
+  (2,	'The Battle of the Dark River',	3),
+  (3,	'White Duck',	1),
+  (4,	'Breaking Barriers',	4),
+  (5,	'The Two Hours',	2);
+  
+ -- COPY AND PASTE INTO BEECROWD FROM THIS POINT ON --
+
+SELECT movie.id, movie.name
+FROM movies movie
+JOIN genres genre ON movie.id_genres = genre.id
+WHERE genre.description = 'Action'
